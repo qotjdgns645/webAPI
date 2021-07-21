f8edef87712e17c6c8e084e2f483914627ce3389
HCNC <HCNC@DESKTOP-98H6RUJ>
2021-07-21 17:55:17 +0900

    7/21 퇴근 commit!

--- pj2/src/main/java/com/myproj/myapp/HomeController.java
+++ pj2/src/main/java/com/myproj/myapp/HomeController.java
@@ -7,8 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.servlet.view.RedirectView;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -24,26 +25,32 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home(ModelAndView mav) throws IOException, ParseException {
+	public ModelAndView home(ModelAndView mav) throws Exception {
 		//restTemplate 사용
 //		RestTemplate restTemplate = new RestTemplate();
 //		response = restTemplate.exchange(Url, HttpMethod.GET);
 		
+		
+		mav.setViewName("home");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/covid", method = RequestMethod.POST)
+	public ModelAndView covid (ModelAndView mav) throws ParseException, IOException {
+		
 		Covid covid = new Covid();
 		String res = covid.covid_19();
 		
-		System.out.println("##### res ##### : "+res);
 		JSONParser parser = new JSONParser();
-		
 		Object obj = parser.parse(res);
 		JSONObject jsonObj = (JSONObject) obj;
 		
-		System.out.println("### : "+ jsonObj);
 		
 		mav.addObject("res", jsonObj);
-		mav.setViewName("home");
+		mav.setViewName("jsonView");
 		
 		return mav;
+		
 	}
 	
 }
--- pj2/src/main/webapp/WEB-INF/views/home.jsp
+++ pj2/src/main/webapp/WEB-INF/views/home.jsp
@@ -3,8 +3,42 @@
 <html>
 <head>
 	<title>Home</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script type="text/javascript">	
+
+	//실행시 무조건 search();실행
+// 	$(function(){
+// 		search();
+// 	});
+	
+	$(document).ready(function(){
+		search();
+	})
+	
+	function search(){
+		$.ajax({
+			url : '/covid', // 호출할 jsp 파일
+	 	    type : 'POST',
+	 	    data : '',
+	 	    datatype : 'json',
+	 	    success : function(data) {
+	 	    	console.log(data.modelAndView.model.res.response.body.items.item);
+	 	   		var sido = data.modelAndView.model.res.response.body.items.item;
+	 	   		
+	 	   		alert(sido[0].sidoNm);
+	 	   
+	 	    },
+	 	    error : function(xhr){
+	 	    	alert(xhr.status +", "+xhr.statusText);
+	 	    }
+	 	});
+	}
+	
+</script>
 </head>
 <body>
-	${res}
+<%-- 	<div id="result">${res}</div> --%>
+	
+
 </body>
 </html>
